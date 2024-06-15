c0927a1322ee26416d43a274a074b8bdbf549314
Tony <economic_luojie@outlook.com>
2024-06-15 12:38:33 +0800

    代码提交

---
+++ flink-pipeline-dependencies/src/main/java/com/example/flinkpipelinedependencies/FlinkPipelineDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.example.flinkpipelinedependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkPipelineDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkPipelineDependenciesApplication.class, args);
+    }
+
+}
---
+++ flink-pipeline-dependencies/src/test/java/com/example/flinkpipelinedependencies/FlinkPipelineDependenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flinkpipelinedependencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkPipelineDependenciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
