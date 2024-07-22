acfeaca7350a04820383639b7f4661d1e3577012
jie.luo2 <2715053558@qq.com>
2024-07-22 17:39:00 +0800

    feat:springboot测试

--- flink-pipeline-tests/flink-generate-api/src/main/java/com/turing/test/DataGenerateApplication.java
+++ flink-pipeline-tests/flink-generate-api/src/main/java/com/turing/test/DataGenerateApplication.java
@@ -1,9 +1,16 @@
 package com.turing.test;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @descr:
+ * @author: Tony
+ * */
+@SpringBootApplication
 public class DataGenerateApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(DataGenerateApplication.class, args);
     }
 }
