b0b15896302871446496da2afbd15d0d8d9b5927
jie.luo2 <2715053558@qq.com>
2024-07-23 11:35:43 +0800

    feat:knife4j-openapi3-jakarta-spring-boot-starter

---
+++ flink-pipeline-tests/flink-generate-api/src/main/java/com/turing/common/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.turing.common;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @desr knife4j-openapi3-jakarta-spring-boot-starter 配置
+ * */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("管理端接口")
+                .pathsToMatch("/api/admin/**") // 根据你的实际路径进行配置
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("后端接口")
+                .pathsToMatch("/api/turing/**") // 根据你的实际路径进行配置
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("接口文档")
+                        //描叙
+                        .description("接口文档")
+                        //版本
+                        .version("v1")
+                        //作者信息，自行设置
+                        .contact(new Contact().name("Tony"))
+                        //设置接口文档的许可证信息
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+
+}
