0906cbc0efff55eb311035eae425aed2abc6e05f
jie.luo2 <2715053558@qq.com>
2024-08-12 10:48:28 +0800

    feat:窗口计算函数|.aggregate(new AverageAggregate())

---
+++ flink-pipeline-tasks/flink20-pipeline-task/src/main/java/com/turing/java/flink20/function/AverageAggregate.java
@@ -0,0 +1,35 @@
+﻿package com.turing.java.flink20.function;
+
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+/**
+ * The accumulator is used to keep a running sum and a count. The {@code getResult} method
+ * computes the average.
+ * input
+ *     .keyBy(<key selector>)
+ *     .window(<window assigner>)
+ *     .aggregate(new AverageAggregate());
+ */
+public class AverageAggregate
+        implements AggregateFunction<Tuple2<String, Long>, Tuple2<Long, Long>, Double> {
+    @Override
+    public Tuple2<Long, Long> createAccumulator() {
+        return new Tuple2<>(0L, 0L);
+    }
+
+    @Override
+    public Tuple2<Long, Long> add(Tuple2<String, Long> value, Tuple2<Long, Long> accumulator) {
+        return new Tuple2<>(accumulator.f0 + value.f1, accumulator.f1 + 1L);
+    }
+
+    @Override
+    public Double getResult(Tuple2<Long, Long> accumulator) {
+        return ((double) accumulator.f0) / accumulator.f1;
+    }
+
+    @Override
+    public Tuple2<Long, Long> merge(Tuple2<Long, Long> a, Tuple2<Long, Long> b) {
+        return new Tuple2<>(a.f0 + b.f0, a.f1 + b.f1);
+    }
+}
---
+++ flink-pipeline-tasks/flink20-pipeline-task/src/main/java/com/turing/java/flink20/pipeline/demo1/IntSerializerSnapshot.java
@@ -0,0 +1,10 @@
+﻿package com.turing.java.flink20.pipeline.demo1;
+
+import org.apache.flink.api.common.typeutils.SimpleTypeSerializerSnapshot;
+import org.apache.flink.api.common.typeutils.base.IntSerializer;
+
+public class IntSerializerSnapshot extends SimpleTypeSerializerSnapshot<Integer> {
+    public IntSerializerSnapshot() {
+        super(() -> IntSerializer.INSTANCE);
+    }
+}
