f1f8d286310044ef2e1b87fbd48697e4b1bf02d3
jie.luo2 <2715053558@qq.com>
2024-08-12 11:52:13 +0800

    feat:Table API & SQL

---
+++ flink-pipeline-tasks/flink20-pipeline-task/src/main/java/com/turing/java/flink20/pipeline/demo2/TableAPIAndSQLExample.java
@@ -0,0 +1,19 @@
+﻿package com.turing.java.flink20.pipeline.demo2;
+
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
+
+public class TableAPIAndSQLExample {
+
+    public static void main(String[] args) {
+        EnvironmentSettings settings = EnvironmentSettings
+                .newInstance()
+                .inStreamingMode()
+                //.inBatchMode()
+                .build();
+
+        TableEnvironment tEnv = TableEnvironment.create(settings);
+
+
+    }
+}
---
+++ flink-pipeline-tasks/flink20-pipeline-task/src/main/java/com/turing/java/flink20/pipeline/demo2/TableAPIAndSQLExample2.java
@@ -0,0 +1,18 @@
+﻿package com.turing.java.flink20.pipeline.demo2;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+/**
+ * Alternatively, users can create a StreamTableEnvironment from an existing StreamExecutionEnvironment to interoperate with the DataStream API.
+ * */
+public class TableAPIAndSQLExample2 {
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+
+    }
+
+}
