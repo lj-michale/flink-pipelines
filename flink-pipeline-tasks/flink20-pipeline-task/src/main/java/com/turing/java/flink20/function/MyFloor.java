8e7224e8c6e25293dc74c7444c6e995f9a879086
jie.luo2 <2715053558@qq.com>
2024-08-19 11:38:02 +0800

    feat:flink|ScalarFunction

---
+++ flink-pipeline-tasks/flink20-pipeline-task/src/main/java/com/turing/java/flink20/function/MyFloor.java
@@ -0,0 +1,30 @@
+﻿package com.turing.java.flink20.function;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import org.apache.flink.table.annotation.DataTypeHint;
+import org.apache.flink.table.functions.ScalarFunction;
+
+/**
+ * public static Table report(Table transactions) {
+ *     return transactions.select(
+ *             $("account_id"),
+ *             call(MyFloor.class, $("transaction_time")).as("log_ts"),
+ *             $("amount"))
+ *         .groupBy($("account_id"), $("log_ts"))
+ *         .select(
+ *             $("account_id"),
+ *             $("log_ts"),
+ *             $("amount").sum().as("amount"));
+ * }
+ * */
+public class MyFloor extends ScalarFunction {
+
+    public @DataTypeHint("TIMESTAMP(3)") LocalDateTime eval(
+            @DataTypeHint("TIMESTAMP(3)") LocalDateTime timestamp) {
+
+        return timestamp.truncatedTo(ChronoUnit.HOURS);
+    }
+
+}
