e739a6f41a603ae50350f112a2953177349afd7f
jie.luo2 <2715053558@qq.com>
2024-08-23 16:02:12 +0800

    feat:flink|flink-connector-datagen|内置的DataGen 连接器，主要是用于生成一些随机数，用于在没有数据源的时候，进行流任务的测试以及性能测试等

---
+++ flink-pipeline-tasks/flink20-pipeline-task/src/main/java/com/turing/java/flink20/pipeline/demo10/DataGeneratorDemo.java
@@ -0,0 +1,39 @@
+﻿package com.turing.java.flink20.pipeline.demo10;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
+import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Flink从1.11开始提供了一个内置的DataGen 连接器，主要是用于生成一些随机数，用于在没有数据源的时候，进行流任务的测试以及性能测试等。
+ * Flink1.17开始提供了新的Source写法
+ * */
+public class DataGeneratorDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataGeneratorSource<String> dataGeneratorSource =
+                new DataGeneratorSource<>(
+                        new GeneratorFunction<Long, String>() {
+                            @Override
+                            public String map(Long value) throws Exception {
+                                return "Number:"+value;
+                            }
+                        },
+                        Long.MAX_VALUE,
+                        RateLimiterStrategy.perSecond(10),
+                        Types.STRING
+                );
+
+        env.fromSource(dataGeneratorSource, WatermarkStrategy.noWatermarks(), "datagenerator").print();
+
+
+        env.execute();
+    }
+}
